7cd9a239bd7cd53674c70720d7c3080000b530e4
aeebbr <aeebbr@gmail.com>
2022-04-20 21:48:16 +0900

    [Refactor] App.js import front

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,16 @@
+import React, { useState, useEffect, useReducer, createContext } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Main from './components/main/Main';
+
 function App() {
 	return (
-		<div>
-			<h1>'아'</h1>
-		</div>
+		<Router>
+			<Routes>
+				{/* 추후 경로 수정 요망 */}
+				<Route path="/" exact element={<Main />} />
+			</Routes>
+		</Router>
 	);
 }
 
