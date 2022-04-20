f6c9dc58837a7457e77ab8ef6958f4ed509c5d44
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-21 06:15:12 +0900

    [Style] kcal 표시 추가

--- front/src/components/forms/FoodInput.js
+++ front/src/components/forms/FoodInput.js
@@ -21,15 +21,24 @@ const FoodInput = () => {
             <InputGroup>
                 <FloatingLabel
                     label="Search Food ..." //
-                    style={{ width: 600, margin: "0 auto" }}
+                    style={{ margin: "0 auto" }}
                     onChange={(event) => setSearch(event.target.value)}
                     onFocus={() => setIsFocus(true)}
                     onBlur={() => setIsFocus(false)}
                 >
-                    <Form.Control type="text" placeholder="Search Food ..." style={{ width: 600 }} />
+                    <Form.Control type="text" placeholder="Search Food ..." style={{ width: 480 }} />
                     {isFocus && (
-                        <Dropdown.Menu show style={{ width: 600, marginTop: -2, padding: 0 }}>
-                            {foods.length > 0 ? foods.map((foods) => <Dropdown.Item key={foods._id}>{foods.name}</Dropdown.Item>) : <Dropdown.Item key={"Can't find."}>검색 결과를 찾을 수 없습니다.</Dropdown.Item>}
+                        <Dropdown.Menu show style={{ width: 480, marginTop: -2, padding: 0 }}>
+                            {foods.length > 0 ? (
+                                foods.map((foods) => (
+                                    <Dropdown.Item key={foods._id}>
+                                        {foods.name}
+                                        <span style={{ float: "right", fontSize: 15, color: "gray" }}>{foods.kcal_per100g} kcals</span>
+                                    </Dropdown.Item>
+                                ))
+                            ) : (
+                                <Dropdown.Item key={"Food is not defined."}>검색 결과를 찾을 수 없습니다.</Dropdown.Item>
+                            )}
                         </Dropdown.Menu>
                     )}
                 </FloatingLabel>
