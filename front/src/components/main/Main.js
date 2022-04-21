54b79eed684e0620b802c60386bf2dca6bf0c2b6
aeebbr <aeebbr@gmail.com>
2022-04-21 15:10:19 +0900

    [Feat] Main 컴포넌트 수정 front

--- front/src/components/main/Main.js
+++ front/src/components/main/Main.js
@@ -1,13 +1,30 @@
-import MainTabs from './MainTabs'
+import React, { useState } from 'react';
+
+import MainTabs from './MainTabs';
+import MainGraph from './MainGraph';
 
 const Main = () => {
+	const [foodSelected, setFoodSelected] = useState('');
+	const [exerciseSelected, setExerciseSelected] = useState('');
+
 	return (
 		<div>
-            <div>안녕하세요 땡땡땡님!</div>
+			<div>안녕하세요 땡땡땡님!</div>
 
-			<MainTabs/>
-        </div>
+			<MainTabs
+				foodSelected={foodSelected}
+				setFoodSelected={setFoodSelected}
+				exerciseSelected={exerciseSelected}
+				setExerciseSelected={setExerciseSelected}
+			/>
+			<MainGraph
+				foodSelected={foodSelected}
+				setFoodSelected={setFoodSelected}
+				exerciseSelected={exerciseSelected}
+				setExerciseSelected={setExerciseSelected}
+			/>
+		</div>
 	);
-}
+};
 
 export default Main;
