81e68a5e245e965058de7d8241c08ac2068c1e4d
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-04-21 16:02:35 +0900

    [Feat] 칼로리 모델에서 _id로 찾기

--- back/src/db/models/Calories.js
+++ back/src/db/models/Calories.js
@@ -1,5 +1,34 @@
 import { CalorieModel } from "../schemas/calories";
 
 class Calories {
-    static create({ newCalories })
+    static async create({ newCalories }){
+        const createdNewCalories = await CalorieModel.create(newCalories);
+        return createdNewCalories
+    }
+
+    static async find({ _id }){
+        const calories = await CalorieModel.find({ _id });
+        return calories
+    }
+
+    static async findByUserId({ userId }){
+        const calories = await CalorieModel.find({ userId });
+        return calories
+    }
+
+    static async update({ _id, fieldToUpdate, newValue }) {
+        const filter = { _id };
+        const update = { [fieldToUpdate]: newValue };
+        const option = { returnOriginal: false };
+
+        const updatedCalorie = await CalorieModel.findOneAndUpdate(
+            filter,
+            update,
+            option
+        );
+        return updatedCalorie;
+    };
+
 }
+
+export { Calories };
