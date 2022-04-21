0f15184d8bdf10d8e29e48681a05292638d858f5
kunnie-code <kearnyyoon@gmail.com>
2022-04-21 23:10:07 +0900

    [Feat] add Mui in the RegistForm.js

--- front/src/components/user/RegisterForm.js
+++ front/src/components/user/RegisterForm.js
@@ -4,6 +4,18 @@ import { useNavigate } from 'react-router-dom';
 
 import * as Api from '../../api';
 
+// Mui
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 function RegisterForm() {
 	const navigate = useNavigate();
 
@@ -15,6 +27,14 @@ function RegisterForm() {
 	const [confirmPassword, setConfirmPassword] = useState('');
 	//useState로 name 상태를 생성함.
 	const [name, setName] = useState('');
+	//useState로 gender 상태를 생성함.
+	const [gender, setGender] = useState('male');
+	//useState로 height 상태를 생성함.
+	const [height, setHeight] = useState('');
+	//useState로 weight 상태를 생성함.
+	const [weight, setWeight] = useState('');
+	//useState로 icon 상태를 생성함.
+	const [icon, setIcon] = useState('runner');
 
 	//이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
 	const validateEmail = email => {
@@ -58,7 +78,170 @@ function RegisterForm() {
 
 	return (
 		<>
-			<div>
+			<Container
+				style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 50 }}
+			>
+				<FormControl action="/" onSubmit={handleSubmit}>
+					<Box
+						sx={{
+							'& > :not(style)': { m: 1, width: '28ch' },
+						}}
+						noValidate
+						autoComplete="off"
+					>
+						<TextField
+							autoFocus
+							required
+							// {!checkLogin && error}
+							// error={!checkLogin}
+							id="outlined-required"
+							label="Email Address"
+							autoComplete="email"
+							// helperText={
+							// 	(!isEmailValid && <span>이메일 형식이 올바르지 않습니다.</span>) ||
+							// 	(!checkLogin && <span>잘못된 이메일입니다.</span>)
+							// }
+							value={email}
+							onChange={e => {
+								setEmail(e.target.value);
+								// setCheckLogin(true);
+							}}
+							// defaultValue="Hello World"
+						/>
+						{!isEmailValid && <p>이메일 형식이 올바르지 않습니다.</p>}
+						<br></br>
+						<TextField
+							required
+							// error={!checkLogin}
+							id="outlined-password-input"
+							label="Password"
+							type="password"
+							autoComplete="current-password"
+							value={password}
+							// helperText={
+							// 	(!isPasswordValid && <span> 비밀번호는 4글자 이상입니다. </span>) ||
+							// 	(!checkLogin && <span>잘못된 비밀번호입니다.</span>)
+							// }
+							onChange={e => {
+								setPassword(e.target.value);
+								// setCheckLogin(true);
+							}}
+						/>
+						{!isPasswordValid && <p>비밀번호는 4글자 이상으로 설정해 주세요.</p>}
+						<br></br>
+						<TextField
+							required
+							// error={!checkLogin}
+							label="Confirm Password"
+							type="password"
+							autoComplete="current-password"
+							value={confirmPassword}
+							// helperText={
+							// 	(!isPasswordValid && <span> 비밀번호는 4글자 이상입니다. </span>) ||
+							// 	(!checkLogin && <span>잘못된 비밀번호입니다.</span>)
+							// }
+							onChange={e => {
+								setConfirmPassword(e.target.value);
+								// setCheckLogin(true);
+							}}
+						/>
+						{!isPasswordSame && <p>비밀번호가 일치하지 않습니다.</p>}
+						<br></br>
+						<TextField
+							required
+							// {!checkLogin && error}
+							// error={!checkLogin}
+							label="Nick Name"
+							// autoComplete="email"
+							// helperText={
+							// 	(!isEmailValid && <span>이메일 형식이 올바르지 않습니다.</span>) ||
+							// 	(!checkLogin && <span>잘못된 이메일입니다.</span>)
+							// }
+							value={name}
+							onChange={e => {
+								setName(e.target.value);
+								// setCheckLogin(true);
+							}}
+							// defaultValue="Hello World"
+						/>
+						{!isNameValid && <p>별명은 2글자 이상으로 설정해 주세요.</p>}
+						<br></br>
+						<FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+						<RadioGroup
+							row
+							aria-labelledby="demo-row-radio-buttons-group-label"
+							name="row-radio-buttons-group"
+							value={gender}
+							onChange={e => setGender(e.target.value)}
+						>
+							<FormControlLabel value="male" control={<Radio />} label="Male" />
+							<FormControlLabel value="female" control={<Radio />} label="Female" />
+						</RadioGroup>
+						<TextField
+							required
+							// {!checkLogin && error}
+							// error={!checkLogin}
+							label="Height"
+							// autoComplete="email"
+							// helperText={
+							// 	(!isEmailValid && <span>이메일 형식이 올바르지 않습니다.</span>) ||
+							// 	(!checkLogin && <span>잘못된 이메일입니다.</span>)
+							// }
+							value={height}
+							onChange={e => {
+								setHeight(e.target.value);
+								// setCheckLogin(true);
+							}}
+							// defaultValue="Hello World"
+						/>
+						<br></br>
+						<TextField
+							required
+							// {!checkLogin && error}
+							// error={!checkLogin}
+							label="Weight"
+							// autoComplete="email"
+							// helperText={
+							// 	(!isEmailValid && <span>이메일 형식이 올바르지 않습니다.</span>) ||
+							// 	(!checkLogin && <span>잘못된 이메일입니다.</span>)
+							// }
+							value={weight}
+							onChange={e => {
+								setWeight(e.target.value);
+								// setCheckLogin(true);
+							}}
+							// defaultValue="Hello World"
+						/>
+						<br></br>
+						<FormLabel id="demo-row-radio-buttons-group-label">Icon</FormLabel>
+						<RadioGroup
+							row
+							aria-labelledby="demo-row-radio-buttons-group-label"
+							name="row-radio-buttons-group"
+							value={icon}
+							onChange={e => setIcon(e.target.value)}
+						>
+							<FormControlLabel value="runner" control={<Radio />} label="runner" />
+							<FormControlLabel value="walker" control={<Radio />} label="walker" />
+							<FormControlLabel value="gymFreak" control={<Radio />} label="gymFreak" />
+							<FormControlLabel value="beginner" control={<Radio />} label="beginner" />
+						</RadioGroup>
+						<Stack spacing={1} direction="row">
+							<Button variant="contained" type="submit" disabled={!isFormValid}>
+								가입하기
+							</Button>
+							<Button variant="contained" onClick={() => navigate('/login')}>
+								로그인
+							</Button>
+							<Button variant="outlined" onClick={() => navigate('/')}>
+								시작페이지
+							</Button>
+						</Stack>
+					</Box>
+				</FormControl>
+			</Container>
+
+			{/* <div>
 				<form onSubmit={handleSubmit}>
 					<div>
 						<label>이메일 주소</label>
@@ -109,7 +292,7 @@ function RegisterForm() {
 					</button>
 					<button onClick={() => navigate('/login')}>로그인 하기</button>
 				</form>
-			</div>
+			</div> */}
 
 			{/* <Container>
 				<Row className="justify-content-md-center mt-5">
