fedb08e92b817b8dc58ca81c8ad3bf64a10badad
aeebbr <aeebbr@gmail.com>
2022-04-21 15:11:11 +0900

    [Feat] Main 운동 탭 자동완성 검색 추가 front

--- front/src/components/main/MainExerciseTab.js
+++ front/src/components/main/MainExerciseTab.js
@@ -1,9 +1,69 @@
-const MainExerciseTab = () => {
+import React, { useState, useMemo, useCallback } from 'react';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+import Autocomplete from '@mui/material/Autocomplete';
+
+const exerciseList = [
+	{
+		value: 'hamburger',
+		label: '걷기',
+	},
+	{
+		value: 'cake',
+		label: '달리기',
+	},
+	{
+		value: 'ham',
+		label: '달리면서 걷기',
+	},
+	{
+		value: 'startham',
+		label: '축구',
+	},
+	{
+		value: 'chicken',
+		label: '야구',
+	},
+];
+
+// const options = ['Option 1', 'Option 2'];
+function MainExerciseTab({ exerciseSelected, setExerciseSelected }) {
+	// inputValue/ onInputChangeprops 조합 으로 "입력 값" 상태 . 이 상태는 텍스트 상자에 표시되는 값을 나타냅니다.
+	const [inputValue, setInputValue] = React.useState('');
+
+	const handleOnClick = () => {
+		console.log(exerciseSelected);
+	};
+
 	return (
 		<div>
-			<div>안녕하세요 운동</div>
+			{/* <div>{`value: ${value !== null ? `'${value}'` : 'null'}`}</div>
+			<div>{`inputValue: '${inputValue}'`}</div>
+			<br /> */}
+			<Autocomplete
+				value={exerciseSelected}
+				onChange={(event, newValue) => {
+					setExerciseSelected(newValue.label);
+				}}
+				inputValue={inputValue}
+				onInputChange={(event, newInputValue) => {
+					setInputValue(newInputValue);
+				}}
+				id="controllable-states-demo"
+				options={exerciseList}
+				sx={{ width: 300 }}
+				renderInput={(params) => <TextField {...params} label="운동" />}
+			/>
+			<Button variant="contained" onClick={handleOnClick}>
+				확인
+			</Button>
 		</div>
 	);
-};
+}
 
 export default MainExerciseTab;
