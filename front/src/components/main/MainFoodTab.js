da8bb209ad3952be068ed1592b410272d09e1806
aeebbr <aeebbr@gmail.com>
2022-04-22 14:43:26 +0900

    [Feat] Main 음식 다중선택 폼으로 변경 front

--- front/src/components/main/MainFoodTab.js
+++ front/src/components/main/MainFoodTab.js
@@ -11,6 +11,7 @@
 import React, { useState, useMemo, useCallback } from 'react';
 
 import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack';
@@ -25,26 +26,32 @@ const foodList = [
 	{
 		value: 'hamburger',
 		label: '햄버거',
+		kcal: 100,
 	},
 	{
 		value: 'cake',
 		label: '케이크',
+		kcal: 300,
 	},
 	{
 		value: 'ham',
 		label: '햄',
+		kcal: 400,
 	},
 	{
 		value: 'startham',
 		label: '햄으로 시작',
+		kcal: 450,
 	},
 	{
 		value: 'chicken',
 		label: '치킨',
+		kcal: 800,
 	},
 	{
 		value: 'fried-chicken',
 		label: '후라이드 치킨',
+		kcal: 520,
 	},
 ];
 
@@ -147,36 +154,66 @@ const foodList = [
 // export default ComboBox;
 
 // const options = ['Option 1', 'Option 2'];
-function MainFoodTab({ foodSelected, setFoodSelected }) {
+function MainFoodTab({ foodSelected, setFoodSelected, IsFoodSelected, setIsFoodSelected }) {
 	// inputValue/ onInputChangeprops 조합 으로 "입력 값" 상태 . 이 상태는 텍스트 상자에 표시되는 값을 나타냅니다.
 	const [inputValue, setInputValue] = React.useState('');
 
-	const handleOnClick = () => {
-		console.log(foodSelected);
-	};
+	// const handleOnClick = () => {
+	// 	console.log(foodSelected);
+	// };
 
 	return (
 		<div>
 			{/* <div>{`value: ${value !== null ? `'${value}'` : 'null'}`}</div>
 			<div>{`inputValue: '${inputValue}'`}</div>
 			<br /> */}
-			<Autocomplete
-				value={foodSelected}
-				onChange={(event, newValue) => {
-					setFoodSelected(newValue.label);
-				}}
-				inputValue={inputValue}
-				onInputChange={(event, newInputValue) => {
-					setInputValue(newInputValue);
-				}}
-				id="controllable-states-demo"
-				options={foodList}
-				sx={{ width: 300 }}
-				renderInput={(params) => <TextField {...params} label="음식" />}
-			/>
-			<Button variant="contained" onClick={handleOnClick}>
+			<div style={{ display: 'flex' }}>
+				<Autocomplete
+					multiple
+					id="controllable-states-demo"
+					value={foodSelected[0]?.label}
+					options={foodList}
+					sx={{ width: 300 }}
+					onChange={(event, newValue) => {
+						setFoodSelected(newValue);
+					}}
+					inputValue={inputValue}
+					onInputChange={(event, newInputValue) => {
+						setInputValue(newInputValue);
+					}}
+					// renderInput={(params) => <TextField {...params} label="음식" />}
+
+					getOptionLabel={(option) => [option.label, `${option.kcal}`]}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							variant="standard"
+							label="Multiple values"
+							placeholder="Favorites"
+						/>
+					)}
+				/>
+				{/* <Box
+					component="form"
+					sx={{
+						'& > :not(style)': { m: 1, width: '25ch' },
+					}}
+					noValidate
+					autoComplete="off"
+				>
+					<TextField
+						id="outlined-basic"
+						// label="그램"
+						variant="outlined"
+						disabled="true"
+						value={100}
+					/>
+				</Box> */}
+			</div>
+			<Button variant="contained" onClick={setIsFoodSelected(true)}>
 				확인
 			</Button>
+			{console.log(foodSelected)}
 		</div>
 	);
 }
