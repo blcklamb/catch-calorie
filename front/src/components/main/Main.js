707e8e383da6941fe2068bb4146916ff24661740
aeebbr <aeebbr@gmail.com>
2022-04-22 14:40:54 +0900

    [Feat] Main 수정/삭제, 상세 버튼 추가 front

--- front/src/components/main/Main.js
+++ front/src/components/main/Main.js
@@ -1,28 +1,42 @@
 import React, { useState } from 'react';
 
+import Button from '@mui/material/Button';
+
 import MainTabs from './MainTabs';
 import MainGraph from './MainGraph';
 
 const Main = () => {
-	const [foodSelected, setFoodSelected] = useState('');
+	const [foodSelected, setFoodSelected] = useState([]);
+	const [IsFoodSelected, setIsFoodSelected] = useState(false);
 	const [exerciseSelected, setExerciseSelected] = useState('');
 
 	return (
 		<div>
 			<div>안녕하세요 땡땡땡님!</div>
 
-			<MainTabs
-				foodSelected={foodSelected}
-				setFoodSelected={setFoodSelected}
-				exerciseSelected={exerciseSelected}
-				setExerciseSelected={setExerciseSelected}
-			/>
-			<MainGraph
-				foodSelected={foodSelected}
-				setFoodSelected={setFoodSelected}
-				exerciseSelected={exerciseSelected}
-				setExerciseSelected={setExerciseSelected}
-			/>
+			<div style={{ display: 'inline-flex' }}>
+				<MainTabs
+					foodSelected={foodSelected}
+					setFoodSelected={setFoodSelected}
+					IsFoodSelected={IsFoodSelected}
+					setIsFoodSelected={setIsFoodSelected}
+					exerciseSelected={exerciseSelected}
+					setExerciseSelected={setExerciseSelected}
+				/>
+				<MainGraph
+					foodSelected={foodSelected}
+					setFoodSelected={setFoodSelected}
+					IsFoodSelected={IsFoodSelected}
+					setIsFoodSelected={setIsFoodSelected}
+					exerciseSelected={exerciseSelected}
+					setExerciseSelected={setExerciseSelected}
+				/>
+			</div>
+			<div>
+				<Button variant="contained">수정 및 삭제</Button>
+				<br />
+				<Button variant="contained">상세보기</Button>
+			</div>
 		</div>
 	);
 };
