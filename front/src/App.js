eff408e2a8280312364ff0dea6f98e76879abdef
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-22 20:05:54 +0900

    Initial Commit

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import FoodInput from "./components/forms/FoodInput";
+
+function App() {
+    return (
+        <>
+            <h1>무지개발자 프론트 개발 끝! 😄</h1>
+            <FoodInput />
+        </>
+    );
+}
+
+export default App;
