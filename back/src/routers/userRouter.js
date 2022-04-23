7b4fb1c75d200d5d539f8e7f2b71e264b9383f96
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-23 21:32:38 +0900

    [Fix] is

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,4 +1,4 @@
-import { is } from "@sindresorhus";
+import is from "@sindresorhus/is";
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
 import { userAuthService } from "../services/userService";
