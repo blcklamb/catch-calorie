47b4061a86dd363f87e0ce0d66c742d7164c5d53
aeebbr <aeebbr@gmail.com>
2022-04-23 11:18:42 +0900

    [Create] MainButton.js 생성, 버튼 스타일 컴포넌트 생성  front

---
+++ front/src/components/main/style/MainButton.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+import Button from '@mui/material/Button';
+
+const MainButton = styled(Button)({
+//   backgroundColor: '#F03E3E',
+//   background: 'linear-gradient(45deg, #da534e 30%, #FF8E53 90%)',
+  background: 'linear-gradient(45deg, #F03E3E 30%, #F03E3E 90%)',
+  border: 0,
+  borderRadius: 20,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+
+  //   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  //   border: 0,
+  //   borderRadius: 20,
+  //   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  //   color: 'white',
+  //   height: 48,
+  //   padding: '0 30px',
+});
+
+export default MainButton;
