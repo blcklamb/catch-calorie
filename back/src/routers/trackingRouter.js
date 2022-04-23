c8723004522d23b2f8c2bc62d6104b332094238e
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-23 21:05:43 +0900

    [Style] 코드 순서 변경

--- back/src/routers/trackingRouter.js
+++ back/src/routers/trackingRouter.js
@@ -4,6 +4,20 @@ import { trackingService } from "../services/trackingService";
 
 const trackingRouter = Router();
 
+trackingRouter.get("/tracking/:user_id", async (req, res, next) => {
+    try {
+        // const { currentUserId } = req;
+        const { user_id } = req.params;
+        const date = new Date().toISOString().split("T")[0];
+
+        const tracking = await trackingService.getTrackingByUserAndDate({ user_id, date });
+
+        return res.status(200).send(tracking);
+    } catch (error) {
+        next(error);
+    }
+});
+
 trackingRouter.post("/tracking/food", async (req, res, next) => {
     try {
         // const { currentUserId } = req;
@@ -20,6 +34,7 @@ trackingRouter.post("/tracking/food", async (req, res, next) => {
 
 trackingRouter.post("/tracking/exer", async (req, res, next) => {
     try {
+        // const { currentUserId } = req;
         const { exer, hour } = req.body;
         const date = new Date().toISOString().split("T")[0];
 
@@ -54,20 +69,6 @@ trackingRouter.delete("/tracking/exer", async (req, res, next) => {
         const deletedTracking = await trackingService.deleteExerTracking({ id });
 
         return res.status(201).json(deletedTracking);
-    } catch (error) {
-        next(error);
-    }
-});
-
-trackingRouter.get("/tracking/:user_id", async (req, res, next) => {
-    try {
-        // const { currentUserId } = req;
-        const { user_id } = req.params;
-        const date = new Date().toISOString().split("T")[0];
-
-        const tracking = await trackingService.getTrackingByUserAndDate({ user_id, date });
-
-        return res.status(200).send(tracking);
     } catch (error) {
         next(error);
     }
