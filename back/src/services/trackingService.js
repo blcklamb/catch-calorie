f7e2a514d1c15b81cc2b2ebacc7e6930d89bc919
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-26 20:01:04 +0900

    [Style]

--- back/src/services/trackingService.js
+++ back/src/services/trackingService.js
@@ -7,6 +7,7 @@ class trackingService {
         const user = await User.findById({ user_id });
         const bmi = user.weight / (user.height / 100) ** 2;
         const rec_cal = (user.height / 100) ** 2 * bmi * 35;
+
         return Tracking.create({ user_id, date, rec_cal });
     }
 
