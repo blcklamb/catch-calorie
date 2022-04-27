ea0111dc4373c63676010f7913f1c1eb3e7bc542
kunnie-code <kearnyyoon@gmail.com>
2022-04-27 09:58:21 +0900

    [Comment] comment in atoms.js

--- front/src/atoms.js
+++ front/src/atoms.js
@@ -3,12 +3,14 @@ import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
 
+// 유저 정보가 담겨있는 아톰
 export const userState = atom({
   key: 'userState',
   default: { user: null },
   effects_UNSTABLE: [persistAtom],
 });
 
+// 유저 로그인시 받는 데이터(토큰이 있는)
 export const tokenState = atom({
   key: 'tokenState',
   default: { token: null },
