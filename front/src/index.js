fa9a5959626d1632dd5b7008aa3edbef717bc8b2
kunnie-code <kearnyyoon@gmail.com>
2022-04-27 11:16:25 +0900

    [Comment] comment in index.js

--- front/src/index.js
+++ front/src/index.js
@@ -6,6 +6,7 @@ import { RecoilRoot } from 'recoil';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* 리코일 관련 컴포넌트 */}
     <RecoilRoot>
       <App />
     </RecoilRoot>
