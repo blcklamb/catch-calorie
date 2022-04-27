f2a5b61aaf88de3601ca924723c2d672a81d4059
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-27 11:54:29 +0900

    [Style] userRouter code style

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -22,27 +22,21 @@ class userAuthService {
             icon,
         };
 
-        const createdNewUser = await User.create({ newUser });
-        return createdNewUser;
+        return User.create({ newUser });
     }
 
     //로그인 시 확인
     static async getUser({ email, password }) {
         const user = await User.findOne({ email });
-        if (!user) {
-            const errorMessage = "가입 내역이 없는 이메일입니다. 다시 한 번 확인해 주세요.";
-            return { errorMessage };
-        }
+        if (!user) return { errorMessage: "가입 내역이 없는 이메일입니다. 다시 한 번 확인해 주세요." };
 
         const isCorrectPassword = await bcrypt.compare(password, user.password);
-        if (!isCorrectPassword) {
-            const errorMessage = "비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.";
-            return { errorMessage };
-        }
+        if (!isCorrectPassword) return { errorMessage: "비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요." };
 
         //로그인 성공 시 JWT 웹 토큰 생성
         const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
         const token = jwt.sign({ user_id: user._id }, secretKey, { expiresIn: "2h" });
+
         const loginUser = {
             token,
             id: user._id,
@@ -64,10 +58,7 @@ class userAuthService {
     //사용자 정보 조회 시
     static async getUserById({ user_id }) {
         const user = await User.findById({ user_id });
-        if (!user) {
-            const errorMessage = "가입 내역이 없는 사용자입니다. 다시 한 번 확인해 주세요.";
-            return { errorMessage };
-        }
+        if (!user) return { errorMessage: "가입 내역이 없는 사용자입니다. 다시 한 번 확인해 주세요." };
         return user;
     }
 }
