a077906a25af4ae397c8c620d199c2aa6047cc5e
aeebbr <aeebbr@gmail.com>
2022-04-27 17:27:09 +0900

    [Feat] 로그인 유저 트래킹 리스트 get, 수정 및 삭제 버튼 생성 front

--- front/src/components/TrackingList.js
+++ front/src/components/TrackingList.js
@@ -1,27 +1,62 @@
 import React, { useState, useEffect } from 'react';
+
 import styled from 'styled-components';
+import Button from '@mui/material/Button';
 
 import * as Api from '../api';
 
-function TrackingList({ foodSelected, setFoodSelected, totalFood, setTotalFood }) {
+function TrackingList({ user }) {
   const [trackingList, setTrackingList] = useState('');
 
-  const id = '626751e37f0a6752662edaf6';
+  const testId = '626751e37f0a6752662edaf6';
+  // const userId = user._id
+  // console.log(userId);
 
   useEffect(() => {
-    Api.get(`tracking/${id}`).then((res) => setTrackingList(res.data));
+    Api.get(`tracking/${testId}`).then((res) => setTrackingList(res.data));
+    // Api.get(`tracking/${user._id}`).then((res) => setTrackingList(res.data));
   }, []);
 
   return (
     <>
-      트래킹 리스트
+      트래킹 리스트 데모
       {/* {console.log(trackingList)} */}
-      {/* {console.log(trackingList.food_record)} */}
-      {/* {console.log(trackingList && trackingList.food_record.map((list) => list))} */}
-      {trackingList &&
-        trackingList.food_record.map((list) => {
-          return [list.food, list.calorie];
-        })}
+      <div>
+        <h1>Food</h1>
+        {trackingList &&
+          trackingList.food_record.map((food) => {
+            return (
+              <div>
+                {food.name}
+                {food.calorie}
+                <Button variant="contained" type="submit">
+                  Modify
+                </Button>
+                <Button variant="contained" type="submit">
+                  Delete
+                </Button>
+              </div>
+            );
+          })}
+      </div>
+      <div>
+        <h1>Exercise</h1>
+        {trackingList &&
+          trackingList.exer_record.map((exercise) => {
+            return (
+              <div>
+                {exercise.name}
+                {exercise.calorie}
+                <Button variant="contained" type="submit">
+                  Modify
+                </Button>
+                <Button variant="contained" type="submit">
+                  Delete
+                </Button>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 }
