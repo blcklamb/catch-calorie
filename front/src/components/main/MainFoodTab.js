47774b7de19a8fc19a1406a919321a099fa9ac96
aeebbr <aeebbr@gmail.com>
2022-04-28 10:28:50 +0900

    [Feat] Food 트래킹 post front

--- front/src/components/main/MainFoodTab.js
+++ front/src/components/main/MainFoodTab.js
@@ -14,8 +14,12 @@ function MainFoodTab({
   kcalPerGram,
   setKcalPerGram,
 }) {
+  const [value, setValue] = React.useState([]);
+  // inputValue/ onInputChangeprops 조합 으로 "입력 값" 상태 . 이 상태는 텍스트 상자에 표시되는 값을 나타냅니다.
+  const [inputValue, setInputValue] = React.useState([]);
+
   const [foodForms, setFoodForms] = useState([0]);
-  const [gram, setGram] = useState([])
+  const [gram, setGram] = useState([]);
 
   const handleAddFoodForm = () => {
     let countArr = [...foodForms];
@@ -26,36 +30,59 @@ function MainFoodTab({
     setFoodForms(countArr);
   };
 
+  console.log(foodSelected);
   const handleTracking = () => {
-    setTotalFood(kcalPerGram.reduce((acc, cur) => acc + cur, totalFood));
-    setFoodSelected([]);
-    setKcalPerGram([]);
-
-    // console.log(foodSelected)
-    // console.log(kcalPerGram)
-
-    // // 백 구현이 되지 않았으므로 일단 중지
-    // foodSelected.map((food, i) => {
-    //   console.log(food.name)
-    //   console.log(food.name)
-    //   const name = food.name
-    //   const gram = kcalPerGram[i] // gram -> kcal
+    foodSelected.map((food, i) => {
+      // const name = food.name;
+      const g = Number(gram[i]);
       
-    //   Api.post(`tracking/food`, {
-    //     name, gram // gram -> kcal
-    //   })
-    // })
+      try {
+        Api.post(`tracking/food`, {
+         name: food.name, 
+         gram: g
+       });
 
-    // Api.post(`tracking/food`, foodSelected[0]?._id); // .then((res) => setFoodApiList(res.data));
+      } catch (err) {
+        console.log('전송 실패', err)
+      }
+
+      Api.post(`foods/${food._id}`);
+    });
+
+    setTotalFood(kcalPerGram.reduce((acc, cur) => acc + cur, totalFood));
+    // setFoodSelected([]);
+    // setGram([]);
+    // setKcalPerGram([]);
+    setFoodSelected(foodSelected.map((f, i) => 0));
+    setGram(gram.map((g, i) => 0));
+    setKcalPerGram(kcalPerGram.map((g, i) => 0));
+
+    // console.log(kcalPerGram)
   };
+
+  // useEffect(() => {
+  //   kcalPerGram.map((kcal, idx) => {
+  //     if(isNaN(kcal)) {
+  //       setKcalPerGram([...kcalPerGram.slice(0, idx), 0, ...kcalPerGram.slice(idx + 1)])
+  //     }
+  //   })
+  // }, [gram])
 
   return (
     <div>
+      {console.log(foodSelected)}
+      {console.log(gram)}
+      {console.log(kcalPerGram)}
+      {/* {console.log(kcalPerGram)} */}
       {foodForms &&
         foodForms.map((item, i) => (
           <MainFoodForm
             key={i}
             idx={i}
+            value={value}
+            setValue={setValue}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
             foodSelected={foodSelected}
             setFoodSelected={setFoodSelected}
             gram={gram}
