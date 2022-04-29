e698c989d2dbcd3dca0202197ef274e63b5b2f52
aeebbr <aeebbr@gmail.com>
2022-04-29 20:31:02 +0900

    [Feat] 햄버거 바 완성

--- front/src/components/HeaderHamburger.js
+++ front/src/components/HeaderHamburger.js
@@ -2,28 +2,19 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import styled from 'styled-components';
-// import { UserStateContext } from '../App';
 import { useRecoilState } from 'recoil';
 import { tokenState, userInfoState, userState } from '../atoms';
 
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const HeaderHamburgerBox = styled.div`
   width: 100vw;
@@ -34,16 +25,10 @@ const HeaderHamburgerBox = styled.div`
   align-items: center;
   position: absolute;
 `;
-// const Avocado = styled.img`
-//   width: 100px;
-//   height: 100px;
-//   margin: 15px;
-// `;
 
 function Header(props) {
   const navigate = useNavigate();
 
-  const [user, setUser] = useRecoilState(userInfoState);
   const [token, setToken] = useRecoilState(tokenState);
   const [recoilUser, setRecoilUser] = useRecoilState(userState);
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
@@ -102,99 +87,34 @@ function Header(props) {
 
   return (
     <HeaderHamburgerBox>
-      {/* <Toolbar> */}
-      {/* <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-                sx={{ mr: 2, display: { sm: 'none' } }}
-              >
-                <MenuIcon />
-              </IconButton>
-            <Box
-              component="nav"
-              sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-              aria-label="mailbox folders"
-            >
-              <Drawer
-                container={container}
-                variant="temporary"
-                open={mobileOpen}
-                onClose={handleDrawerToggle}
-                ModalProps={{
-                  keepMounted: true, // Better open performance on mobile.
-                }}
-                sx={{
-                  display: { xs: 'block', sm: 'none' },
-                  '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                }}
-              >
-                {drawer}
-              </Drawer>
-              <Drawer
-                variant="permanent"
-                sx={{
-                  display: { xs: 'none', sm: 'block' },
-                  '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                }}
-                open
-              >
-                {drawer}
-              </Drawer>
-            </Box> */}
-      {/* <AppBar
-              position="fixed"
-              sx={{
-                width: '100%',
-                ml: { sm: `${drawerWidth}px` },
-              }}
-            >
-              <Toolbar> */}
-      <IconButton style={{ paddingRight: '30px'}}
+      <IconButton
+        style={{ paddingRight: '30px' }}
         color="inherit"
         aria-label="open drawer"
         edge="start"
         onClick={handleDrawerToggle}
         sx={{ mr: 2 }}
-        // sx={{ mr: 2, display: { sm: 'none' } }}
       >
         <MenuIcon />
       </IconButton>
-      {/* <Typography variant="h6" noWrap component="div">
-                  Responsive drawer
-                </Typography>
-              </Toolbar>
-            </AppBar> */}
-      {/* <Box component="nav" sx={{ width: { sm: drawerWidth } }} aria-label="mailbox folders"> */}
-      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-      <Drawer 
-        // container={container}
+      <Drawer
+        container={container}
+        anchor={'right'}
         variant="temporary"
         open={mobileOpen}
         onClose={handleDrawerToggle}
         ModalProps={{
-          keepMounted: true, // Better open performance on mobile.
+          keepMounted: true,
         }}
         sx={{
           display: { xs: 'block' },
           // display: { xs: 'block', sm: 'none' },
           '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
         }}
+        style={{ zIndex: 4000 }}
       >
         {drawer}
       </Drawer>
-      {/* <Drawer
-                variant="permanent"
-                sx={{
-                  display: { xs: 'none', sm: 'block' },
-                  '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                }}
-                open
-              >
-                {drawer}
-              </Drawer> */}
-      {/* </Box> */}
     </HeaderHamburgerBox>
   );
 }
