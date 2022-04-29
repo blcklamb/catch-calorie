8087ee229f947568b16fd0ad73c513029b7a964d
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-04-29 13:01:44 +0900

    [Style] code formatting

--- back/src/services/trackingService.js
+++ back/src/services/trackingService.js
@@ -13,9 +13,9 @@ class trackingService {
 
     static async addFoodTracking({ user_id, date, name, gram }) {
         const calorie = await Food.findByName({ name })
-            .then((data) =>  data.kcal_per_100g) //kcal_per_100g
-            .then((cal_per_100g) => cal_per_100g / 100) //kcal_per_g
-            .then((cal_per_1g) => cal_per_1g * gram);           
+            .then((data) => data.kcal_per_100g) //kcal_per_100g
+            .then((kcal_per_100g) => kcal_per_100g / 100) //kcal_per_g
+            .then((kcal_per_1g) => kcal_per_1g * gram);
 
         const toUpdate = {
             $push: { food_record: { id: uuid(), name, gram, calorie } },
