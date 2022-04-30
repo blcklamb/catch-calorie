653aa536f62e8720b8b64df29dc3cfb338aee76b
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-04-30 21:38:08 +0900

    [Fix] food unit change complete

--- back/src/services/foodService.js
+++ back/src/services/foodService.js
@@ -1,8 +1,8 @@
 import { Food } from "../db";
 
 class foodService {
-    static addFood({ category, name, kcal_per_100g }) {
-        return Food.create({ newFood: { category, name, kcal_per_100g } });
+    static addFood({ category, name, kcal_per_100g, kcal_per_lb}) {
+        return Food.create({ newFood: { category, name, kcal_per_100g, kcal_per_lb } });
     }
 
     static getFoodByName({ name }) {
