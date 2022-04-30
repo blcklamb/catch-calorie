4e97c60c098c5f0db2ec4b8840655c10e0626c8e
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-04-30 17:07:29 +0900

    [Create] awardRouter.js

--- back/src/services/awardService.js
+++ back/src/services/awardService.js
@@ -1,12 +1,18 @@
 import { Award } from "../db";
 
 class awardService {
+    static async addAward({ user_id }) {
+        const userAward = await Award.findByUser({ user_id });
+        if (userAward) return { errorMessage: "awardService 이미 생성된 award입니다" };
+
+        return Award.create({ user_id });
+    }
 
     static getAwardByUser({ user_id  }) {
         return Award.findByUser({ user_id });
     }
 
-    static async deleteAward({ user_id }) {
+    static deleteAward({ user_id }) {
         return Award.delete({ user_id });
     }
 }
