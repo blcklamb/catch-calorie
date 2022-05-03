253fde40dedd4760f8b04b1a3bbb2278985f841b
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-05-03 17:20:08 +0900

    [Fix] upload_image.js

--- back/src/middlewares/upload_image.js
+++ back/src/middlewares/upload_image.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { url } from "url";
 var AWS = require('aws-sdk');
 
-const BUCKET_NAME = process.env.BUCKET_NAME
+//const BUCKET_NAME = process.env.BUCKET_NAME
 const BUCKET_URL = 'bucket-5ialfb.s3.ap-northeast-2.amazonaws.com'
 
 const myConfig = new AWS.Config();
@@ -15,14 +14,17 @@ myConfig.update({
 const bucketRouter = Router()
 
 bucketRouter.get('/imgTest', async (req, res, next)=> {
-    //const {pathname} = url.parse(req.url, true)
     try{
         const { icon } = req.body
         return res.redirect(`https://${BUCKET_URL}/icon/${icon}`)
-    } catch( error) {
+    } catch(error) {
         next(error)
     }
     
 })
+
+// const getIcon(res) {
+
+// }
 
 export {bucketRouter}
