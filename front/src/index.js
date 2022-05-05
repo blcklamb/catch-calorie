a2381ea702b52cda8576f0d6aa919efe8283fa2a
kunnie-code <kearnyyoon@gmail.com>
2022-05-05 23:39:46 +0900

    [Style] decided max-width

--- front/src/index.js
+++ front/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-    </BrowserRouter>
+    <Container sx={{ maxWidth: '1440px' }}>
+      <BrowserRouter>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </BrowserRouter>
+    </Container>
   </React.StrictMode>,
 );
