14535ebfd4beeae8bf162d40c97e8c660be917df
aeebbr <aeebbr@gmail.com>
2022-05-07 03:20:39 +0900

    [Design] Add food 스타일링

---
+++ front/src/components/styledCompo/Add.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import { LoginGlass, TitleText, RedSpan } from '../styledCompo/LoginStyle';
+
+export const AddGlassBodyContainer = styled.div``;
+export const AddFormsContainer = styled.div`
+  /* margin-bottom: 20px; */
+`;
+
+export const AddTitle = styled(TitleText)`
+  padding-top: 80px;
+  margin-bottom: 70px;
+`;
+
+export const AddFormSection = styled.div`
+  margin: 40px 0px;
+`;
+
+export const AddFormTitle = styled.h1``;
+
+export const AddButtonContainer = styled.div`
+  margin: 30px 0px;
+`;
