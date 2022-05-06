5b0f2a42163cd242c0dc94e05df0dc9af72aa169
aeebbr <aeebbr@gmail.com>
2022-05-06 12:57:41 +0900

    [Design] TrackingForm layout 수정

--- front/src/components/styledCompo/mainStyle.js
+++ front/src/components/styledCompo/mainStyle.js
@@ -10,41 +10,36 @@ import Button from '@mui/material/Button';
 const COLORS = ['#5bc691', '#FFBB28', '#C66868', '#FF8042'];
 
 export const BodyContainer = styled.div`
-    width: 100%; 
+  width: 100%;
 
-    background: pink;
+  background: pink;
 
-
-
-    align-content: center;
-    justify-content: center;
-    margin-top: 180px;
-    margin-bottom: 180px;
-    display: grid;
+  align-content: center;
+  justify-content: center;
+  margin-top: 180px;
+  margin-bottom: 180px;
+  display: grid;
 `;
 
 export const MainHelloSection = styled.div`
-width: 100%;
-padding: 20px 20px;
+  width: 100%;
+  padding: 20px 20px;
 
-background: yellow;
-display: flex;
-align-items: center;
-    height: 150px;
-    align-content: center;
-    position: relative;
+  background: yellow;
+  display: flex;
+  align-items: center;
+  height: 150px;
+  align-content: center;
+  position: relative;
 `;
 
 export const MainSection1 = styled.div`
-
-display: inline-flex;
-margin: 80px 0px;
-
-
+  display: inline-flex;
+  margin: 80px 0px;
 `;
 
 export const Section = styled.div`
-height: 100%;
+  height: 100%;
   position: relative;
   padding: 30px 40px;
   left: 0%;
@@ -63,26 +58,26 @@ height: 100%;
 
 // 그래프 ----------------------------------------------------------------------------------------
 export const GraphContainer = styled.div`
-width: 400px;
+  width: 400px;
 `;
 
 // 탭 ----------------------------------------------------------------------------------------
 export const TrackingForm = styled.div`
-height: 80px; 
-display: flex;
-margin: 20px 0;
+  /* height: 80px;  */
+  display: flex;
+  margin: 20px 0;
 `;
 
 export const TrackingAutoContainer = styled.div`
-width: 300px;
-margin-right: 20px;
+  width: 300px;
+  margin-right: 20px;
 `;
 
 export const TrackingTextFieldContainer = styled.div`
-width: 150px;
-margin-right: 20px;
+  width: 150px;
+  margin-right: 20px;
 
-/* component="form"
+  /* component="form"
 sx={{
 '& > :not(style)': { m: 1, width: '25ch' },
 }}
@@ -94,112 +89,14 @@ export const TrackingSwitchContainer = styled.div`
   float: right;
   flex-grow: 1;
   text-align: right;
-
 `;
 
 export const TrackingButtonContainer = styled.div`
-width: 100%;
-margin-top:40px;
-text-align: center;
+  width: 100%;
+  margin-top: 40px;
+  text-align: center;
 `;
 
 export const TrackingPlusButtonContainer = styled.div`
-margin-bottom: 60px;
-`
-
-// export const FirstPage = styled.section`
-//   height: 100vh;
-//   display: flex;
-
-//   align-items: center;
-//   justify-content: center;
-//   flex-direction: column;
-// `;
-
-// const FirstPageWrapper = styled.div`
-//   position: absolute;
-//   z-index: 500;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   flex-direction: column;
-//   padding-top: 200px;
-// `;
-
-// const MainButton = styled(Button)({
-//   background: 'linear-gradient(45deg, #F03E3E 30%, #F03E3E 90%)',
-//   border: 0,
-//   borderRadius: 20,
-//   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//   color: 'white',
-// });
-
-// const CircleRed1 = styled.div`
-//   position: absolute;
-//   z-index: 300;
-//   width: 470.49px;
-//   height: 470.49px;
-//   left: 1589px;
-//   top: 1000.51px;
-//   border-radius: 50%;
-//   background: radial-gradient(
-//       87.63% 87.63% at 30.82% 78.65%,
-//       #c62e2e 0%,
-//       #e44545 29.02%,
-//       #f39999 69.13%
-//     )
-//     /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
-//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-// `;
-
-// const CircleGreen1 = styled.div`
-//   position: absolute;
-
-//   right: -55px;
-//   z-index: 300;
-//   width: 700px;
-//   height: 700px;
-//   left: -330.39px;
-//   top: 1800px;
-//   border-radius: 50%;
-//   background: radial-gradient(
-//     87.63% 87.63% at 30.82% 78.65%,
-//     rgba(105, 156, 29, 0.9) 0%,
-//     #77bb41 29.02%,
-//     #d6eeb1 69.13%
-//   );
-//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-// `;
-// const CircleRed2 = styled.div`
-//   position: absolute;
-//   width: 200px;
-//   height: 200px;
-//   left: 830.75px;
-//   top: 3079.47px;
-
-//   border-radius: 50%;
-//   background: radial-gradient(
-//       87.63% 87.63% at 30.82% 78.65%,
-//       #c62e2e 0%,
-//       #e44545 29.02%,
-//       #f39999 69.13%
-//     )
-//     /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
-//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-// `;
-// const CircleGreen2 = styled.div`
-//   position: absolute;
-
-//   width: 700px;
-//   height: 700px;
-//   left: 600px;
-//   top: 3500px;
-//   border-radius: 50%;
-//   background: radial-gradient(
-//     87.63% 87.63% at 30.82% 78.65%,
-//     rgba(105, 156, 29, 0.9) 0%,
-//     #77bb41 29.02%,
-//     #d6eeb1 69.13%
-//   );
-//   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-// `;
+  margin-bottom: 60px;
+`;
