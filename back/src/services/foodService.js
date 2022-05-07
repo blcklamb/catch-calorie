41f36f85b2ca6ee2000e90473dabb0a55d386a25
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-05-07 11:08:45 +0900

    [Style] code formatting

--- back/src/services/foodService.js
+++ back/src/services/foodService.js
@@ -25,7 +25,7 @@ class foodService {
         return Food.update({ id, toUpdate: { $inc: { views: 1 } } }, { new: true });
     }
 
-    static async convertUnit({kcal, unit}) {
+    static async convertUnit({ kcal, unit }) {
         let kcal_per_lb = kcal;
         let kcal_per_100g = kcal;
 
@@ -39,7 +39,6 @@ class foodService {
 
         return { kcal_per_100g, kcal_per_lb };
     }
-
 }
 
 export { foodService };
