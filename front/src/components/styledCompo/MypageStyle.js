4e2dcd237a2a5f60be8b9d97bfa2bc93ae707250
MongLong0214 <weplay0628@gmail.com>
2022-05-07 20:59:18 +0900

    [Design] header avocado del

--- front/src/components/styledCompo/MypageStyle.js
+++ front/src/components/styledCompo/MypageStyle.js
@@ -1,12 +1,29 @@
 import styled from 'styled-components';
 
-export const MypageCircleRed = styled.div`
+export const MypageCircleGreen = styled.div`
   z-index: 50;
   position: absolute;
   width: 700px;
   height: 700px;
-  left: 60%;
+  right: 0.1%;
   top: 58%;
+  border-radius: 50%;
+  background: radial-gradient(
+    87.63% 87.63% at 30.82% 78.65%,
+    rgba(105, 156, 29, 0.9) 0%,
+    #77bb41 29.02%,
+    #d6eeb1 69.13%
+  );
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const MypageCircleRed = styled.div`
+  position: absolute;
+  z-index: 50;
+  width: 350px;
+  height: 350px;
+  left: 7%;
+  top: 49%;
 
   border-radius: 50%;
   background: radial-gradient(
@@ -16,23 +33,6 @@ export const MypageCircleRed = styled.div`
       #f39999 69.13%
     )
     /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-export const MypageCircleGreen = styled.div`
-  position: absolute;
-  z-index: 50;
-  width: 350px;
-  height: 350px;
-  left: 7%;
-  top: 49%;
-  border-radius: 50%;
-  background: radial-gradient(
-    87.63% 87.63% at 30.82% 78.65%,
-    rgba(105, 156, 29, 0.9) 0%,
-    #77bb41 29.02%,
-    #d6eeb1 69.13%
-  );
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
