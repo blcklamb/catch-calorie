e3f7ad0e62211fc7e2183c674e446cdb5673da81
aeebbr <aeebbr@gmail.com>
2022-05-07 16:27:46 +0900

    [Design] 트래킹 리스트 조건부 렌더링 작업 중

--- front/src/components/trackingList/TrackingFoodList.js
+++ front/src/components/trackingList/TrackingFoodList.js
@@ -162,43 +162,7 @@ function TrackingFoodList({ food, isTrackingPage }) {
           </TrackingListTdEnd>
         </TrackingListTr>
       ) : (
-        // <div style={{ display: 'flex' }}>
-        //   <div style={{ display: 'flex' }}>
-        //     <div style={{ marginRight: '30px' }}>{food.name}</div>
-        //     <ValidationTextField
-        //       id="outlined-name"
-        //       label="weight"
-        //       value={weight}
-        //       onChange={onChange}
-        //       style={{ marginRight: '30px' }}
-        //       helperText={
-        //         !isWeightNumber ? (
-        //           <span>Please enter a number only</span>
-        //         ) : (
-        //           isWeightEmpty && <span>Please enter a weight</span>
-        //         )
-        //       }
-        //     />
-        //     <Switch
-        //       checked={checked}
-        //       onChange={handleSwitch}
-        //       inputProps={{ 'aria-label': 'controlled' }}
-        //     />
-        //     {unit === 'us' ? 'US standard' : 'metric'}
-        //     <div style={{ marginRight: '30px' }}>
-        //       {previewKcal()}kcal/{unit === 'us' ? 'lb' : 'g'}
-        //     </div>
-        //   </div>
-        //   <div>
-        //     <Button variant="contained" type="button" onClick={handleCheck}>
-        //       Check
-        //     </Button>
-        //     <Button variant="contained" type="button" onClick={handleCancel}>
-        //       Cancel
-        //     </Button>
-        //   </div>
-        // </div>
-        <TrackingListTr>
+        <TrackingListTr isTrackingPage={isTrackingPage}>
           <TrackingListTdStart>{food.name}</TrackingListTdStart>
           <TrackingListTd>{food.gram}g</TrackingListTd>
           <TrackingListTd>{food.calorie}kcal</TrackingListTd>
